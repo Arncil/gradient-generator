7e63eb4805fa3128f751df28dad93fb6d237be57
Arncil <jackowiakmilosz@gmail.com>
2022-12-11 04:03:00 +0100

    Add some functionality

--- src/Components/GradientGenerator/GradientGenerator.js
+++ src/Components/GradientGenerator/GradientGenerator.js
@@ -8,11 +8,12 @@ export class GradientGenerator extends React.Component {
         super(props)
 
         this.state = {
+            visible: true,
             angle: 180,
-            hue: 120,
+            hue: Math.floor(Math.random() * 360),
             saturation: 50,
             lightness: 50,
-            hue2: 240,
+            hue2: Math.floor(Math.random() * 360),
             saturation2: 50,
             lightness2: 50
         }
@@ -55,32 +56,48 @@ export class GradientGenerator extends React.Component {
         })
     }
     setBackground = () => {
-        return `linear-gradient(${this.state.angle}deg,
-                hsl(${this.state.hue}, ${this.state.saturation}%, ${this.state.lightness}%),
-                hsl(${this.state.hue2}, ${this.state.saturation2}%, ${this.state.lightness2}%))`
+        return `linear-gradient(${this.state.angle}deg, hsl(${this.state.hue}, ${this.state.saturation}%, ${this.state.lightness}%), hsl(${this.state.hue2}, ${this.state.saturation2}%, ${this.state.lightness2}%))`
     }
     setRandomGradient = () => {
         this.setState({
-            angle: Math.floor(Math.random()*360),
-            hue: Math.floor(Math.random()*360),
-            hue2: Math.floor(Math.random()*360),
-            saturation: Math.floor(Math.random()*100),
-            saturation2: Math.floor(Math.random()*100),
-            lightness: Math.floor(Math.random()*100),
-            lightness2: Math.floor(Math.random()*100)
+            angle: Math.floor(Math.random() * 360),
+            hue: Math.floor(Math.random() * 360),
+            hue2: Math.floor(Math.random() * 360),
+            saturation: Math.floor(Math.random() * 100),
+            saturation2: Math.floor(Math.random() * 100),
+            lightness: Math.floor(Math.random() * 100),
+            lightness2: Math.floor(Math.random() * 100)
         })
     }
     setTextColor = () => {
-        return (this.state.lightness+this.state.lightness2)/2 > 50 ? '#000' : '#fff'
+        return (this.state.lightness + this.state.lightness2) / 2 > 50 ? '#000' : '#fff'
+    }
+    copyOnClick = () => {
+        navigator.clipboard.writeText(this.setBackground())
+        alert("Copied: " + this.setBackground())
     }
     render() {
+        const hideShowText = this.state.visible === true ? '⚙️ Hide Settings ⚙️' : '⚙️ Show Settings ⚙️'
+        const hideShowElement = (
+            <div className='flex element' style={{ background: this.setBackground(), color: this.setTextColor(), cursor: 'pointer' }}
+                onClick={() => { this.setState({ visible: !this.state.visible }) }}>
+                <h3>{hideShowText}</h3>
+            </div>
+        )
+        if (this.state.visible === false) {
+            return (
+                <div className='gradient-generator grid' style={{ background: this.setBackground() }}>
+                    {hideShowElement}
+                </div>
+                )
+        }
         return (
-            <div className='gradient-generator grid' style={{ background: this.setBackground()}}>
+            <div className='gradient-generator grid' style={{ background: this.setBackground() }}>
                 <div>
-                    <h2 className='flex element' style={{ background: this.setBackground(), color: this.setTextColor()}} >Gradient Generator</h2>
+                    <h2 className='flex element' style={{ background: this.setBackground(), color: this.setTextColor() }} >Gradient Generator</h2>
                     <Angle onChangeAngle={this.changeAngle} angle={this.state.angle}
-                    hue={this.state.hue} saturation={this.state.saturation} lightness={this.state.lightness}
-                    hue2={this.state.hue2} saturation2={this.state.saturation2} lightness2={this.state.lightness2}/>
+                        hue={this.state.hue} saturation={this.state.saturation} lightness={this.state.lightness}
+                        hue2={this.state.hue2} saturation2={this.state.saturation2} lightness2={this.state.lightness2} />
 
                     <div className='flex-colors'>
                         <Color
@@ -102,13 +119,17 @@ export class GradientGenerator extends React.Component {
                             lightness={this.state.lightness2}
                         />
                     </div>
-                    <div className='grid element' style={{ background: this.setBackground(), color: this.setTextColor()}}>
+                    <div className='grid element' style={{ background: this.setBackground(), color: this.setTextColor(), cursor: 'pointer' }}
+                        onClick={this.copyOnClick}>
                         <h3>Code:</h3>
+                        <h5>Click to copy</h5>
                         <p>{this.setBackground()}</p>
                     </div>
-                    <div className='flex element' style={{ background: this.setBackground(), color: this.setTextColor()}} onClick={this.setRandomGradient}>
+                    <div className='flex element' style={{ background: this.setBackground(), color: this.setTextColor(), cursor: 'pointer' }}
+                        onClick={this.setRandomGradient}>
                         <h3>Random Gradient</h3>
                     </div>
+                    {hideShowElement}
                 </div>
             </div>
         )
